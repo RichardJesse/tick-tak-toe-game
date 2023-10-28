f750f66f1f269a2fafd895de4c42f50b39a84b15
gradhav001 <jesse.richard@strathmore.edu>
2023-10-28 16:58:35 +0300

    added the multiplayer functionality only some design work need

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 const express = require('express');
-
-const app = express()
-
 const path = require('path');
-const http =require('https');
 
-const {Server} = require('socket.io');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const server = http.createServer(app);
- const io =  new Server(server);
+let rooms = 0;
 
- app.use(express.static(path.resolve('')));
+app.use(express.static('.'));
 
- app.get('/', (req,res) => {
-    return res.sendFile('index.html')
- })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'game.html'));
+});
 
- sever.listen(3000,()=>{
-    console.log('port connected to port 3000')
- })
+io.on('connection', (socket) => {
 
+    // Create a new game room and notify the creator of game.
+    socket.on('createGame', (data) => {
+        socket.join(`room-${++rooms}`);
+        socket.emit('newGame', { name: data.name, room: `room-${rooms}` });
+    });
 
+    // Connect the Player 2 to the room he requested. Show error if room full.
+    socket.on('joinGame', function (data) {
+        var room = io.nsps['/'].adapter.rooms[data.room];
+        if (room && room.length === 1) {
+            socket.join(data.room);
+            socket.broadcast.to(data.room).emit('player1', {});
+            socket.emit('player2', { name: data.name, room: data.room })
+        } else {
+            socket.emit('err', { message: 'Sorry, The room is full!' });
+        }
+    });
 
+    /**
+       * Handle the turn played by either player and notify the other.
+       */
+    socket.on('playTurn', (data) => {
+        socket.broadcast.to(data.room).emit('turnPlayed', {
+            tile: data.tile,
+            room: data.room
+        });
+    });
+
+    /**
+       * Notify the players about the victor.
+       */
+    socket.on('gameEnded', (data) => {
+        socket.broadcast.to(data.room).emit('gameEnd', data);
+    });
+});
+
+server.listen(process.env.PORT || 5000);
